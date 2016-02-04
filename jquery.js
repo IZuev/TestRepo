a254caf8e9743d3bfd571b036ef7416063b2c103
Igor <dex543@yandex.com>
2016-02-04 20:00:35 +0300

    js

--- jquery.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function(){
-
-    $(".btn-slide").click(function(){
-        $("#panel").slideToggle("slow");
-        $(this).toggleClass("active"); return false;
-    });
-
-});
---
+++ js/jquery.js
@@ -0,0 +1,13 @@
+(function($, undefined)
+{
+
+    $(function () {
+        $("h2").css("color", "red");
+        $(".ssh").attr({
+            'href':'http://anton.shevchuk.name',
+            'title':'My Personal Blog'
+        });
+    });
+
+})(jQuery);
+
